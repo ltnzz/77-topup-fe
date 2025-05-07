8013f25c54face16f98d2891dde0e1c06ad8523c
accoustic guitar <benitaaryani123@gmail.com>
2025-05-07 14:13:23 +0700

    Fitur footer (#6)
    * footer terbaru
    
    * footer terbaru

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,12 +5,14 @@ import { Outlet } from "react-router";
 
 export const RootLayout = () => {
   return (
-    <>
+      <div className="flex flex-col min-h-screen">
       <nav>
         <Navbar />
       </nav>
+      <main className="flex-1">
       <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
